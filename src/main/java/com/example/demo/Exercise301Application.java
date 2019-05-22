e18b287d5e76beacb39e06cfa1e0ce018a12c309
GBTC406013UR <GBTC406013UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-05-22 16:22:22 -0400

    Exercise301

---
+++ src/main/java/com/example/demo/Exercise301Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise301Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exercise301Application.class, args);
+	}
+
+}
